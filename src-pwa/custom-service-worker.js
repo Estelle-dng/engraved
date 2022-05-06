f1a35b919bce7153fdbdb931b454236678694863
Estelle <estelle.denage@gmail.com>
2022-05-06 20:33:55 +0200

    add cache strategies

--- src-pwa/custom-service-worker.js
+++ src-pwa/custom-service-worker.js
@@ -4,6 +4,51 @@
  * quasar.conf > pwa > workboxPluginMode is set to "InjectManifest"
  */
 
-import {precacheAndRoute} from 'workbox-precaching';
+/*
+  dependencies
+*/
 
-precacheAndRoute(self.__WB_MANIFEST);
+  import {precacheAndRoute} from 'workbox-precaching'
+  import {registerRoute} from 'workbox-routing'
+  import {StaleWhileRevalidate} from 'workbox-strategies'
+  import {CacheFirst} from 'workbox-strategies'
+  import {ExpirationPlugin} from 'workbox-expiration'
+  import {CacheableResponsePlugin} from 'workbox-cacheable-response'
+  import {NetworkFirst} from 'workbox-strategies';
+  
+  
+/*
+  config
+*/
+
+  precacheAndRoute(self.__WB_MANIFEST);
+
+/*
+  caching strategies
+*/
+
+  registerRoute(
+    ({url}) => url.host.startsWith('use.typekit.net'),
+   
+    new CacheFirst({
+      cacheName: 'adobe-fonts',
+      plugins: [
+        new ExpirationPlugin({
+          maxEntries: 30,
+        }),
+        new CacheableResponsePlugin({
+          statuses: [0, 200]
+        }),
+      ],
+    })
+  );
+
+  registerRoute(
+    ({url}) => url.pathname.startsWith('/posts'),
+    new NetworkFirst()
+  );
+
+  registerRoute(
+    ({url}) => url.href.startsWith('http'),
+    new StaleWhileRevalidate()
+  );
